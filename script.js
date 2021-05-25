a2d10fdf6ac24b61d0bb23db8ac9f7f6562792ec
attila <attilavigh.contact@gmail.com>
2021-05-26 00:24:40 +0100

    Refractory:  Ex 9, 10, 11
    Ex.9: The position of the paragraphs changed
    Ex.10: forEach method replaced with for.. of
    Ex.11: From toggle click from all to specific one

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 // EX7.: Write a function to change the h1 text
+
 const changeH1 = () => document.getElementsByTagName("h1")[0].innerText = "Puppy Land"
 changeH1()
+
 // EX8.: Write a function to change the page background color
+
 const changePageBackgroundColor = () => document.body.style.backgroundColor = "#00bcd4"
 changePageBackgroundColor()
+
 // EX9.: Write a function to change the footer address with a fake one
+
 const changeAddres = () => {
-    let footerParagraphs = document.querySelectorAll("footer>section>p");
-    footerParagraphs[1].innerText = "High Heaven"
-    footerParagraphs[2].innerText = "777"
+    let footerParagraphs = document.querySelectorAll("footer>section>p"); // Get the list of paragraphs
+    footerParagraphs[0].innerText = "High Heaven"
+    footerParagraphs[1].innerText = "777"
 }
 changeAddres()
+
 // EX10.: Write a function to add a CSS class to every Amazon link
-const addClassTOAmazonLinks = () => (
-    document.querySelectorAll("a").forEach( 
-        link => link.classList.add("amazon") 
-    )
-)
-addClassTOAmazonLinks()
-// EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
-const toggleClasses = () => {
-    let images = document.querySelectorAll("img")
-    for (const image of images) {
-        image.classList.toggle("image")        
-    }
+
+const addClassTOAmazonLinks = () => {
+    let links = document.querySelectorAll("a")   // Get the list of links
+
+    for (const link of links)                    // Loop through each link
+        link.classList.add("amazon")             // Add .amazon class to the current link
 }
-toggleClasses()
+addClassTOAmazonLinks()
+
+// EX11: Write a function to toggle a CSS class for all the trs in the table; that class should set the visibility of the image
+
+const addToggleClassToImageOnAllImages = () => {
+    let images = document.querySelectorAll("img")               // Get the list of links
+    
+    for (const image of images)                                 // Loop through each link
+        image.onclick = () => image.classList.toggle("image")   // Each object (image) from the list, each image has a property/attribute named onclick, which will receive the value of a function 
+}
+addToggleClassToImageOnAllImages()
+
+// EX12: Write a function totr the price of each product in a different one every time it's invoked
+
+
+
+
+
