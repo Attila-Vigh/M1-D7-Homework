9c490040173bb9442f79013455f7a5b74e199c9f
attila <attilavigh.contact@gmail.com>
2021-05-25 14:58:37 +0100

    Exercise  8 added
    Write a function to change the page background color

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // EX7.: Write a function to change the h1 text
 const changeH1 = () => document.getElementsByTagName("h1")[0].innerText = "Puppy Land"
 changeH1()
+// EX8.: Write a function to change the page background color
+const changePageBackgroundColor = () => document.body.style.backgroundColor = "#00bcd4"
+changePageBackgroundColor()
